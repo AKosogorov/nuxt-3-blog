c250644f8817b4424f55139fd00b115b113e1066
Anton <kosogorov.anton@gmail.com>
2023-06-18 23:03:45 +0500

    TheHeader

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,33 @@
 <template>
+  <TheHeader />
   <main>
     <slot/>
   </main>
 </template>
 
 <script setup lang="ts">
+import TheHeader from "~/components/TheHeader.vue";
+
+interface IScript {
+  defer: boolean
+  tagPosition: 'bodyClose'
+}
+const options: IScript = {
+  defer: true,
+  tagPosition: 'bodyClose'
+}
+
 useHead({
   script: [
     // Vendor JS Files
-    { src: 'vendor/bootstrap/js/bootstrap.bundle.min.js', defer: true, async: true },
-    { src: 'vendor/glightbox/js/glightbox.min.js', defer: true, async: true },
-    { src: 'vendor/isotope-layout/isotope.pkgd.min.js', defer: true, async: true },
-    { src: 'vendor/swiper/swiper-bundle.min.js', defer: true, async: true },
-    { src: 'vendor/php-email-form/validate.js', defer: true, async: true },
+    { src: 'vendor/bootstrap/js/bootstrap.bundle.min.js', ...options },
+    { src: 'vendor/glightbox/js/glightbox.min.js', ...options },
+    { src: 'vendor/isotope-layout/isotope.pkgd.min.js', ...options },
+    { src: 'vendor/swiper/swiper-bundle.min.js', ...options },
+    { src: 'vendor/php-email-form/validate.js', ...options },
 
     // Template Main JS File
-    { src: 'js/main.js', defer: true, async: true },
+    { src: 'js/main.js', ...options },
   ]
 })
 </script>
